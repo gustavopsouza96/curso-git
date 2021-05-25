bf7d5aaf459098cb0f79600ba78c38c44e8000d2
Gustavo Pinsegher <gustavopinsegher@gmail.com>
2021-05-25 10:06:27 -0300

    Projeto adicionado a pasta

---
+++ 100.001 - Projeto-teste/100.001 - Projeto-teste/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _100._001___Projeto_teste {
+    class Program {
+        static void Main(string[] args) {
+            
+            Console.WriteLine("Olá Mundo!");
+            Console.WriteLine("Primeiro grande teste");
+            Console.WriteLine("Teste sequência");
+
+            Console.ReadLine();
+
+        }
+    }
+}
